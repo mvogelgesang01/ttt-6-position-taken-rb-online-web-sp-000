794ed55ba6dc478448cc91af3346e430afe2908e
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-08 22:52:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,10 @@
 def {position_taken?(board, index)}.to_not raise_error
   cells[inputs.to_i-1]
-  
+  cells.each do |character|
+    if character == "X" || character == "0"
+      true
+    else
+      false
+    end
+  end
+end
